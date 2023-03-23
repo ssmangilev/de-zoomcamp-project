dcde2daa0ec1c81ff693cfeb608e446f4f1971be
Sergey Mangilev <ssmangilev@gmail.com>
2023-03-23 20:01:06 +0100

    ci/cd test commit

--- tests/test_cicd.py
+++ tests/test_cicd.py
@@ -1,8 +1,3 @@
 def test_cicd():
     # This test is to ensure that the CICD pipeline is working
     assert True
-
-
-def test_cicd1():
-    # This test is to ensure that the CICD pipeline is working
-    assert True
