1b5d91d1ca81d4d32238bb4c08f131eb40da2c2b
Sergey Mangilev <ssmangilev@gmail.com>
2023-03-24 20:09:25 +0100

    Fix: Loading historical data fix

--- build/dags/dwd_data_ingestion_gcs_dag_v1.py
+++ build/dags/dwd_data_ingestion_gcs_dag_v1.py
@@ -48,69 +48,60 @@ def dwd_historical_data_v1():
         an AirflowConfigException will be raised.
         """
         context = get_current_context()
-        dag_run = context.get('dag_run')
-        if dag_run and hasattr(dag_run, "conf"):
-            conf = dag_run.conf
-            years_string = conf.get('years_string')
-            filename = conf.get('filename')
-            if not years_string or not filename:
-                raise AirflowConfigException(
-                    "There aren't required parameters in DAG-run."
-                    "Please check configuration of your run.")
-            return f"{DATASET_URL}{years_string}/{filename}"
-
-    @task()
-    def read_data_from_txt(src: str) -> pd.DataFrame:
-        """Reads data from a txt-file
-        and returns pandas.DataFrame as a result
-        """
-        df = pd.read_csv(src)
-        return df
+        params = context.get('params', {})
+        years_string = params.get('years_string')
+        filename = params.get('filename')
+        if not years_string or not filename:
+            raise AirflowConfigException(
+                "There aren't required parameters in DAG-run."
+                "Please check configuration of your run.")
+        return f"{DATASET_URL}{years_string}/{filename}"
 
     @task()
     def read_filename_from_context() -> str:
         """Reads filename variable from DAG-run context"""
         context = get_current_context()
-        return context.get('filename')
+        params = context.get('params', {})
+        return params.get('filename')
 
     @task()
     def get_parquet_filename() -> str:
         """Returns filename"""
         context = get_current_context()
-        filename = context.get('filename')
+        params = context.get('params', {})
+        filename = params.get('filename')
         return filename.replace(".txt", ".parquet")
 
     @task()
     def write_data_from_dataframe_to_parquet(
-            df: pd.DataFrame, filename: str) -> str:
+            src: str, filename: str) -> str:
         """
         Writes data from a pandas.DataFrame to parquet file
         , saves it local and returns a local path
         """
-        save_path = f"{PATH_TO_LOCAL_HOME}{filename}.parquet"
+        df = pd.read_csv(src, encoding='latin-1')
+        save_path = f"{PATH_TO_LOCAL_HOME}/{filename}"
         table = Table.from_pandas(df)
         parquet.write_table(table, save_path)
         return save_path
 
     download_filename = make_download_url()
     filename = read_filename_from_context()
-    wget_bash = f"wget {download_filename} -O {PATH_TO_LOCAL_HOME}{filename}"
+    wget_bash = f"wget {download_filename} -O {PATH_TO_LOCAL_HOME}/{filename}"
     extract = BashOperator(
         task_id="download_file_from_url",
         bash_command=wget_bash
     )
-    dataframe = read_data_from_txt(
-        f"{PATH_TO_LOCAL_HOME}{filename}")
     parquet_filename = get_parquet_filename()
     local_path_to_parquet = write_data_from_dataframe_to_parquet(
-        dataframe, parquet_filename)
+        f"{PATH_TO_LOCAL_HOME}/{filename}", parquet_filename)
     loading = LocalFilesystemToGCSOperator(
         task_id="upload_file_to_cgs",
         src=f"{PATH_TO_LOCAL_HOME}/{parquet_filename}",
         dst=f"data/historical_temperatures/{parquet_filename}",
         bucket=BUCKET)
 
-    download_filename >> extract >> dataframe >> parquet_filename >> local_path_to_parquet >> loading # NOQA
+    download_filename >> extract >> parquet_filename >> local_path_to_parquet >> loading # NOQA
 
 
 dwd_historical_data_v1()
